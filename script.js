7cdbba5a36ad99c4b978b747e850e15df79a2dd5
nihalDevidas <117895185+nihalDevidas@users.noreply.github.com>
2023-07-15 23:03:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const rotateLine = ()=>{
+
+	const lineElement = document.createElement("div");
+
+       lineElement.id = "line";
+       lineElement.classList.add("divPro");
+	
+	document.body.appendChild(lineElement);
+}
+ rotateLine();
